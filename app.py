e6fc17622ad93cb08725b0444575425b6155fe4e
KavGoat <86602648+KavGoat@users.noreply.github.com>
2025-02-05 01:51:06 +0000

    Update

--- app.py
+++ app.py
@@ -24,27 +24,55 @@ st.markdown("""
     </style>
     """, unsafe_allow_html=True)
 
-
 def fetch_data():
     try:
-        data = pd.read_csv('https://docs.google.com/spreadsheets/d/' + 
-                       '1M4VT4eXXPj5UL7Xn8s5B1yu_taULnGQ3jHKzmG14rIA' +
-                       '/export?gid=0&format=csv',
-                       index_col=0
-                      )
-        return data.reset_index().values.tolist()
+        data = pd.read_csv('https://docs.google.com/spreadsheets/d/' +
+                           '1M4VT4eXXPj5UL7Xn8s5B1yu_taULnGQ3jHKzmG14rIA' +
+                           '/export?gid=0&format=csv',
+                           index_col=0
+                           )
+        data = data.reset_index().values.tolist()
+        courses = []
+        for round in data:
+            if round[1] not in courses:
+                courses.append(round[1])
+
+        course_layouts = {"All": "-"}
+
+        for course in courses:
+            layouts = []
+            for round in data:
+                if round[1] == course and round[2] not in layouts:
+                    layouts.append(round[2])
+            if len(layouts) > 1:
+                layouts = ["All"] + layouts
+            course_layouts[course] = layouts
+        return data, course_layouts
     except:
         pass
 
+def update_data():
+    st.markdown("""
+    <script>
+        document.getElementById('tabs-bui2-tab-0').setAttribute('aria-selected','True');
+        document.getElementById('tabs-bui2-tab-1').setAttribute('aria-selected','False');
+        document.getElementById('tabs-bui2-tabpanel-1').setAttribute('hidden','');
+        document.getElementById('tabs-bui2-tabpanel-0').removeAttribute('hidden','False');
+    </script>
+    """, unsafe_allow_html=True)
+    st.rerun()
+
 tab1, tab2 = st.tabs(["Scores", "Update Data"])
 
-y=0
-
 with tab1:
+    data, course_layouts = fetch_data()
+    Course = st.selectbox(
+        "Select a course", list(course_layouts.keys()))
 
-    ting = np.arange(y)
+    Layout = st.selectbox(
+        "Select a layout", course_layouts[Course])
 
-    for x in ting:
+    """for x in ting:
         with st.container(border=True):
             st.write(str(x))
             col1, col2, col3 = st.columns(3)
@@ -55,45 +83,43 @@ with tab1:
             with col3:
                 st.write("Mahith")
             with st.expander("See scores for each hole"):
-                st.write("Data")
+                st.write("Data")"""
 
 with tab2:
-    data = fetch_data()
+    data,course_layouts = fetch_data()
     uploaded_file = st.file_uploader("Choose a file")
     if uploaded_file is not None:
-        dates1 = []
-        
+        dates = []
         for tingle in data:
-            if isinstance(tingle[3], str) and tingle[3] not in dates1:
-                dates1 += [tingle[3]]
-  
+            if isinstance(tingle[3], str) and tingle[3] not in dates:
+                dates += [tingle[3]]
+
         dfd = pd.read_csv(uploaded_file)
-        
+
         new_data = dfd.reset_index().values.tolist()
-        
-        
+
         data_to_append = []
-            
+
         for ting1 in new_data:
-            if ting1[4] not in dates1 and isinstance(ting1[4], str):
+            if ting1[4] not in dates and isinstance(ting1[4], str):
                 data_to_append += [ting1[1:]]
-        
+
         append_clean = [
-            [None if isinstance(x, float) and math.isnan(x) else x for x in row]
+            [None if isinstance(x, float) and math.isnan(x)
+             else x for x in row]
             for row in data_to_append
         ]
-        
+
         SCOPES = [
-                "https://www.googleapis.com/auth/spreadsheets",
-                "https://www.googleapis.com/auth/drive"
-            ]
-          
+            "https://www.googleapis.com/auth/spreadsheets",
+            "https://www.googleapis.com/auth/drive"
+        ]
 
         credentials_info = {
             "type": "service_account",
             "project_id": "udisc-448721",
-            "private_key_id": st.secrets["gapi"]["gapi_id"],  
-            "private_key": st.secrets["gapi"]["gapi_key"],  
+            "private_key_id": st.secrets["gapi"]["gapi_id"],
+            "private_key": st.secrets["gapi"]["gapi_key"],
             "client_email": "udisc-790@udisc-448721.iam.gserviceaccount.com",
             "client_id": "108793919915702574689",
             "auth_uri": "https://accounts.google.com/o/oauth2/auth",
@@ -102,17 +128,18 @@ with tab2:
             "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/udisc-790%40udisc-448721.iam.gserviceaccount.com",
             "universe_domain": "googleapis.com"
         }
-        credentials = Credentials.from_service_account_info(credentials_info, scopes=SCOPES)
-            
+        credentials = Credentials.from_service_account_info(
+            credentials_info, scopes=SCOPES)
+
         gc = gspread.authorize(credentials)
-            
+
         sh = gc.open_by_key("1M4VT4eXXPj5UL7Xn8s5B1yu_taULnGQ3jHKzmG14rIA")
-            
+
         sheet = sh.worksheet("Data")
-            
-            
-            # Append data to the end of the sheet
+
+        # Append data to the end of the sheet
         f = StringIO()
         with redirect_stdout(f):
-            
+
             sheet.append_rows(append_clean, value_input_option="USER_ENTERED")
+        st.link_button("Update", "https://udiscgolf.streamlit.app/")
